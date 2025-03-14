3e1b2c04da3e0e03e309232b7208a0223cc4c7c8
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-03-14 15:33:55 +0100

    Add full icon URL generation and update weather display logic

--- WeatherPage.xaml.cs
+++ WeatherPage.xaml.cs
@@ -4,8 +4,10 @@ namespace WeatherApp;
 
 public partial class WeatherPage : ContentPage
 {
+	public List<Models.List> WeatherList;
 	public WeatherPage()
 	{
+		WeatherList = new List<Models.List>();
 		InitializeComponent();
 	}
 
@@ -13,11 +15,17 @@ public partial class WeatherPage : ContentPage
 	{
 		base.OnAppearing();
 		var result = await ApiService.GetWeather(47.6829, -122.1209);
+		foreach(var item in result.list)
+		{
+			WeatherList.Add(item);
+		}
+		CvlWeather.ItemsSource = WeatherList;
 
 		lblCity.Text = result.city.name;
 		lblWeatherDescription.Text = result.list[0].weather[0].description;
 		lblTemperature.Text = result.list[0].main.Temperature + "°C";
 		lblHumidity.Text = result.list[0].main.humidity + "%";
 		lblWindSpeed.Text = result.list[0].wind.speed + "Km/h";
+		imgWeatherIcon.Source = result.list[0].weather[0].fullIconUrl;
 	}
 }
